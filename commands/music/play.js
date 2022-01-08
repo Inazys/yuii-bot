3aa3dbf16c21ad917c26fd6a71ba42c7b3d4cfdd
Inazys <52779256+Inazys@users.noreply.github.com>
2022-01-08 21:41:27 +0700

    change play command for better performance

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,5 +1,5 @@
 const { QueryType } = require('discord-player');
-
+const playdl = require("play-dl");
 module.exports = {
     name: 'play',
     aliases: ['p'],
@@ -9,7 +9,6 @@ module.exports = {
 
     async execute(client, message, args) {
         if (!args[0]) return message.channel.send(`Please enter a valid search ${message.author}... try again ? ❌`);
-
         const res = await player.search(args.join(' '), {
             requestedBy: message.member,
             searchEngine: QueryType.AUTO
@@ -24,7 +23,21 @@ module.exports = {
             initialVolume: 80,
             leaveOnEmptyCooldown: 60 * 1000 * 3,
             leaveOnEmpty: true,
-            spotifyBridge: true,
+            disableVolume: true,
+            async onBeforeCreateStream(track, source, queue) {
+                let vid;
+                try {
+                    if(track.url.includes("youtube.com"))
+                        vid = (await playdl.stream(track.url)).stream;
+                    else
+                        vid = (await playdl.stream(await playdl.search(`${track.author} ${track.title} lyric`, { limit : 1, source : { youtube : "video" } }).then(x => x[0].url))).stream;
+                } catch {
+                    queue.metadata.send("Error while searching for song!");
+                    vid = (await playdl.stream("https://www.youtube.com/watch?v=Wch3gJG2GJ4", { quality: 0 })).stream; // a 1 second video. if u have a better way to do this, feel free to open a PR/issue :)
+                }
+                return vid;
+            }
+
         });
 
         try {
